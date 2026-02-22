af446307b3df8a6b44e050cab159b6157864c029
Alex L <9060360+AlexLamond@users.noreply.github.com>
2026-02-22 17:07:03 +0100

    Add localization phrases for English language

---
+++ localization/en/index.php
@@ -0,0 +1,32 @@
+<?php
+
+$phrases["latest_blog_post"] = "Latest Blog Posts";
+$phrases["developer_of_xtravel"] = "Developer of xTravel";
+$phrases["photography"] = "Enjoys taking a photo or two";
+$phrases["posted_by"] = "Posted by";
+$phrases["on_word"] = "on";
+$phrases["xtravel_description"] = "xTravel - Public Transport app";
+$phrases["github"] = "Github";
+$phrases["continue_blog"] = "Continue reading...";
+
+$phrases["about"] = "About";
+$phrases["just_now"] = "Just Now";
+$phrases["ago"] = "ago";
+
+$phrases["dates"]["year"] = "year";
+$phrases["dates"]["month"] = "month";
+$phrases["dates"]["week"] = "week";
+$phrases["dates"]["day"] = "day";
+$phrases["dates"]["hour"] = "hour";
+$phrases["dates"]["minute"] = "minute";
+$phrases["dates"]["second"] = "second";
+
+$phrases["dates"]["years"] = "years";
+$phrases["dates"]["months"] = "months";
+$phrases["dates"]["weeks"] = "weeks";
+$phrases["dates"]["days"] = "days";
+$phrases["dates"]["hours"] = "hours";
+$phrases["dates"]["minutes"] = "minutes";
+$phrases["dates"]["seconds"] = "seconds";
+
+?>

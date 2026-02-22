12a105108456cb010a3c520f4430c970d8ffc48e
Alex L <9060360+AlexLamond@users.noreply.github.com>
2026-02-22 17:07:40 +0100

    Add Danish localization phrases

---
+++ localization/dk/index.php
@@ -0,0 +1,32 @@
+<?php
+
+$phrases["latest_blog_post"] = "Seneste blogindlæg";
+$phrases["developer_of_xtravel"] = "Udvikler af xTravel";
+$phrases["photography"] = "Kan lide at tage et foto eller to";
+$phrases["posted_by"] = "Indsendt af";
+$phrases["on_word"] = "på";
+$phrases["xtravel_description"] = "xTravel - app til offentlig transport";
+$phrases["github"] = "Github";
+$phrases["continue_blog"] = "Fortsæt læsning...";
+
+$phrases["about"] = "For ca.";
+$phrases["just_now"] = "Bare nu";
+$phrases["ago"] = "siden";
+
+$phrases["dates"]["year"] = "år";
+$phrases["dates"]["month"] = "måned";
+$phrases["dates"]["week"] = "uge";
+$phrases["dates"]["day"] = "dag";
+$phrases["dates"]["hour"] = "time";
+$phrases["dates"]["minute"] = "minut";
+$phrases["dates"]["second"] = "sekund";
+
+$phrases["dates"]["years"] = "år";
+$phrases["dates"]["months"] = "måneder";
+$phrases["dates"]["weeks"] = "uger";
+$phrases["dates"]["days"] = "dage";
+$phrases["dates"]["hours"] = "timer";
+$phrases["dates"]["minutes"] = "minutter";
+$phrases["dates"]["seconds"] = "sekunder";
+
+?>

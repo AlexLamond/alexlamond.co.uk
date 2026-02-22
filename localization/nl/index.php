7da4e0bced91a96500e178da05be979a3b039232
Alex L <9060360+AlexLamond@users.noreply.github.com>
2026-02-22 17:08:35 +0100

    Add Dutch localization

---
+++ localization/nl/index.php
@@ -0,0 +1,32 @@
+<?php
+
+$phrases["latest_blog_post"] = "Laatste blogberichten";
+$phrases["developer_of_xtravel"] = "Ontwikkelaar van xTravel";
+$phrases["photography"] = "Neemt graag een foto of twee";
+$phrases["posted_by"] = "Gepost door";
+$phrases["on_word"] = "op";
+$phrases["xtravel_description"] = "xTravel - Openbaar Vervoer app";
+$phrases["github"] = "Github";
+$phrases["continue_blog"] = "Lees verder...";
+
+$phrases["about"] = "Ongeveer";
+$phrases["just_now"] = "Zojuist";
+$phrases["ago"] = "geleden";
+
+$phrases["dates"]["year"] = "jaar";
+$phrases["dates"]["month"] = "maand";
+$phrases["dates"]["week"] = "week";
+$phrases["dates"]["day"] = "dag";
+$phrases["dates"]["hour"] = "uur";
+$phrases["dates"]["minute"] = "minuut";
+$phrases["dates"]["second"] = "second";
+
+$phrases["dates"]["years"] = "jaren";
+$phrases["dates"]["months"] = "maanden";
+$phrases["dates"]["weeks"] = "weken";
+$phrases["dates"]["days"] = "dagen";
+$phrases["dates"]["hours"] = "uren";
+$phrases["dates"]["minutes"] = "minuten";
+$phrases["dates"]["seconds"] = "seconden";
+
+?>
